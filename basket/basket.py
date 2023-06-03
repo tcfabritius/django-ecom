e9048e889ab4fcbe024a3e8b1071d0946f12d5bc
tcfabritius <tim.fabritius@skiff.com>
2023-06-03 11:44:11 +0300

    6th commit

--- basket/basket.py
+++ basket/basket.py
@@ -13,3 +13,21 @@ class Basket():
         if 'skey' not in request.session:
             basket = self.session['skey'] = {}
         self.basket = basket
+
+
+    def add(self, product, qty):
+        '''
+        Adding and updating the users basket session data
+        '''
+        product_id = product.id
+        if product_id not in self.basket:
+            self.basket[product_id] = {'price': str(product.price), 'qty': int(qty)}
+
+        self.session.modified = True
+
+    
+    def __len__(self):
+        '''
+        Get the basket data and count the qty of items
+        '''
+        return sum(item['qty'] for item in self.basket.values())
