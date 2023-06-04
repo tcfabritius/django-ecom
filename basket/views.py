a33c56589ce18ca120cff24d4c3213a5fe5730bc
tcfabritius <tim.fabritius@skiff.com>
2023-06-04 11:30:11 +0300

    7th commit

--- basket/views.py
+++ basket/views.py
@@ -6,7 +6,8 @@ from .basket import Basket
 from store.models import Product
 
 def basket_summary(request):
-    return render(request, 'store/basket/summary.html')
+    basket = Basket(request)
+    return render(request, 'store/basket/summary.html', {'basket': basket})
 
 
 def basket_add(request):
@@ -18,4 +19,15 @@ def basket_add(request):
         basket.add(product=product, qty=product_qty)
         basketqty = basket.__len__()
         response = JsonResponse({'qty':basketqty})
+        return response
+    
+
+def basket_delete(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        basket.delete(product=product_id)
+        basketqty = basket.__len__()
+        baskettotal = basket.get_total_price()
+        response = JsonResponse({'qty':basketqty, 'subtotal':baskettotal})
         return response
